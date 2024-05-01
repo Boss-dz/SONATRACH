66faec3177b1a98923e645c5987c627894b3d2e7
rayanMELZI <rayanmelzi@outlook.com>
2024-05-01 23:14:29 +0100

    just fixing the toggle component to be static again , temporary of course

--- src/features/AdminFormation/components/ToggleProfile.jsx
+++ src/features/AdminFormation/components/ToggleProfile.jsx
@@ -1,34 +1,10 @@
 import style from "./ToggleProfile.module.css";
-import { useState, useEffect } from "react";
 
 export default function ToggleProfile() {
-  const [activeSection, setActiveSection] = useState(null);
-
-  useEffect(() => {
-    window.addEventListener("scroll", handleScroll);
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
-
   return (
     <div className={style.container}>
-      <h3 className={style.active}>
-        <a
-          href="#section1"
-          className={activeSection === "section1" ? "active" : ""}
-        >
-          Profile
-        </a>
-      </h3>
-      <h3>
-        <a
-          href="#section2"
-          className={activeSection === "section2" ? "active" : ""}
-        >
-          Application
-        </a>
-      </h3>
+      <h3 className={style.active}>Profile</h3>
+      <h3>Application</h3>
     </div>
   );
 }
