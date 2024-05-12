0758252f24fb0a200b0b1661295f7d73f240237c
rayanMELZI <rayanmelzi@outlook.com>
2024-05-12 20:43:51 +0100

    Finishing Admin IT Part front-end

--- src/features/AdminIT/components/Membres.jsx
+++ src/features/AdminIT/components/Membres.jsx
@@ -1,18 +1,109 @@
-import React from "react";
+import React, { useState } from "react";
 import style from "./Membres.module.css";
 import Titre from "./Titre";
-import QuestDetails from "./QuestDetails";
-// import MoreVertIcon from "@mui/icons-material/MoreVert";
+import RolesMembresList from "./RolesMembresList";
+
+const roles = ["Participant", "Admin Formation", "Admin IT", "Admin Visiteur"];
 
 function VerticalDotsIcon() {
+  const [isDroped, setIsDroped] = useState(false);
+
+  let a = 0;
+  let b = 0;
+  let c = 0;
+  let d = 0;
+  while (a === b || a === c || a === d || b === c || b === d || c === d) {
+    a = Math.random();
+    b = Math.random();
+    c = Math.random();
+    d = Math.random();
+  }
+
   return (
     <div
       style={{
-        fontSize: "20px",
-        fontWeight: "900",
+        position: "relative",
       }}
     >
-      &#8942;
+      <div
+        onClick={() => setIsDroped((d) => !d)}
+        style={{
+          fontSize: "20px",
+          fontWeight: "900",
+          cursor: "pointer",
+        }}
+      >
+        &#8942;
+      </div>
+
+      <form
+        className={style.form}
+        style={isDroped ? { display: "flex" } : { display: "none" }}
+        onMouseLeave={() => {
+          setIsDroped((drop) => !drop);
+        }}
+      >
+        {roles.map((role) => (
+          <>
+            <input
+              className={style.input}
+              type="checkbox"
+              id={`${role.replace(/\s/g, "")}-${a}`}
+              name="roles"
+              value={role.replace(/\s/g, "")}
+            />
+            <label
+              className={style.label}
+              htmlFor={`${role.replace(/\s/g, "")}-${a}`}
+            >
+              {role.replace(/ /g, "\u00A0")}
+            </label>
+          </>
+        ))}
+        {/* <input
+          className={style.input}
+          type="checkbox"
+          id={`Participant${a}`}
+          name="roles"
+          value="Participant"
+        />
+        <label className={style.label} htmlFor={`Participant${a}`}>
+          Participant
+        </label>
+
+        <input
+          className={style.input}
+          type="checkbox"
+          id={`AdminFormation${b}`}
+          name="roles"
+          value="AdminFormation"
+        />
+        <label className={style.label} htmlFor={`AdminFormation${b}`}>
+          Admin&nbsp;Formation
+        </label>
+
+        <input
+          className={style.input}
+          type="checkbox"
+          id={`AdminIT${c}`}
+          name="roles"
+          value="AdminIT"
+        />
+        <label className={style.label} htmlFor={`AdminIT${c}`}>
+          Admin IT
+        </label>
+
+        <input
+          className={style.input}
+          type="checkbox"
+          id={`AdminVisiteur${d}`}
+          name="roles"
+          value="AdminVisiteur"
+        />
+        <label className={style.label} htmlFor={`AdminVisiteur${d}`}>
+          Admin Visiteur
+        </label> */}
+      </form>
     </div>
   );
 }
@@ -26,16 +117,19 @@ function Membres({ roleFilter, setRoleFilter }) {
         size="small"
       />
       <div className={style.container}>
-        <QuestDetails
-          columns={["Nom", "Prénom", "", "Roles"]}
+        <RolesMembresList
+          columns={["Nom", "Prénom", "Roles"]}
           propData={[
             {
               nom: "Rayane",
               prenom: "MELZI",
-              vide1: "",
-              vide2: "",
               action: <VerticalDotsIcon />,
-              roles: ["Participant", "Admin Formation"],
+              roles: [
+                "Participant",
+                "Admin Formation",
+                "Admin IT",
+                "Admin Visiteur",
+              ],
             },
             {
               nom: "Mohammed",
